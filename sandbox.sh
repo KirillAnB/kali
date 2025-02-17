4e923a2e709022ff140d7402260bb26940fbaa44
KirillAnB <it-reso@mail.ru>
2025-02-17 15:37:17 +0300

    scripts

--- sandbox.sh
+++ sandbox.sh
@@ -1,19 +1,6 @@
 #!/bin/bash +x
 
 
-echo "Starting..."
-sleep 1
-while true; do
-	read -p "Enter a file name: " file_name
-	if [[ -z $file_name ]]; then
-		echo 'Enter file name pls: '
-		continue
-	elif [[ -f $file_name ]]; then
-	echo "File is found"
-	break
-	else
-	echo "file was not found"
-	break
-fi
-done
+read -p "Enter value a and value b:" value_a 
 
+echo "${value_a:-42}"
