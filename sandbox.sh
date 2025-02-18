a1c472643cb3d7c5de39c0f634088cce6f713c89
KirillAnB <it-reso@mail.ru>
2025-02-18 15:32:43 +0300

    upper/lower

--- sandbox.sh
+++ sandbox.sh
@@ -1,6 +1,18 @@
 #!/bin/bash +x
 
 
-read -p "Enter value a and value b:" value_a 
+read -p "Enter a word: " word
 
-echo "${value_a:-42}"
+first_low="${word,}"
+all_low="${word,,}"
+first_capital="${word^}"
+all_capital="${word^^}"
+
+echo "original word is: $word
+      all low: $all_low
+      first low: $first_low
+      first capital $first_capital
+      all capital: $all_capital"
+
+
+
