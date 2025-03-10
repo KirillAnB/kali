1023f1be56aec53d50cfa6d20081623f9eab0ffe
KirillAnB <it-reso@mail.ru>
2025-03-10 13:18:56 +0300

    sand

--- sand.sh
+++ sand.sh
@@ -1,20 +1,7 @@
 #! /bin/bash
 
-echo "lets work with read and reply"
-sleep 1
+file=$1
 
-while true; do
-	echo -n "Enter your name: "
-	read name
-	sleep 1
-	echo -n	"Hi $name!One more time or no?('exit' for quit)"
-	read 
-	if [[ $REPLY == "exit" ]]; then
-		echo "Goodbye!"
-		sleep 1
-		break
-	else
-		continue
-	fi
+cat $file | while read line; do
+	echo $line
 done
-
